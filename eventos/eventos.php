2bb7adbbd522bf0aacfac7bc7eaea4634353501e
Dayancmm <Dayan.montanez@payco.co>
2021-09-20 10:57:21 -0500

    Se corrige javascript

--- eventos/eventos.php
+++ eventos/eventos.php
@@ -1,10 +1,10 @@
 <?php
-$fechaIni = date('Y-m-d');
-$mod_date = strtotime($fechaIni."- 5 days");
-$fechaFin =  date("Y-m-d",$mod_date);
-echo $urlws_busqueda = "https://www.banxico.org.mx/SieAPIRest/service/v1/seriesSF43718/datos/".$fechaIni.'/'.$fechaFin;
+$fechaFin = date('Y-m-d');
+$mod_date = strtotime($fechaFin."- 5 days");
+$fechaIni =  date("Y-m-d",$mod_date);
+$urlws_busqueda = "https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/".$fechaIni.'/'.$fechaFin;
 $ch = curl_init($urlws_busqueda);                          
-curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 curl_setopt($ch, CURLOPT_AUTOREFERER, true);
@@ -15,7 +15,31 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Accept: application/json', 
     'Access-Control-Allow-Origin: http://localhost',
     'Bmx-Token: a441355af61c510d2ba5076729a15d8756783e6c2fdbc88a21f3c5d64c956027'));
-$result = curl_exec($ch);
-$redirectURL = curl_getinfo($ch,CURLINFO_REDIRECT_URL);
-print_r($result);
-
+$data = curl_exec($ch);
+$curl_errno = curl_errno($ch);
+$headerResponse = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+$arrayData = array();
+if (!$curl_errno) {
+    $arrayData['json'] = json_decode($data, true);
+    $arrayData['headerCode'] = $headerResponse;
+} else {
+    $arrayData['json'] = $curl_errno;
+    $arrayData['headerCode'] = 500;
+    $arrayData['error'] = $headerResponse;
+}
+if ($arrayData['headerCode'] == 200) {
+       
+    foreach ($arrayData['json']['bmx']['series'] as $data) {
+       $serie = $data['idSerie'];
+       $titulo = $data['titulo'];
+       $html  = '<p>La Serie'.$serie.' de'. $titulo .' contiene los siguientes datos: <br> </p>';
+        $html  .= '<table style="border: 1px solid black;"><tr><td style="border: 1px solid black;">Fecha</td><td style="border: 1px solid black;">Tipo de cambio</td>';
+       foreach ($data['datos'] as $val) {          
+           $html .= '<tr><td style="border: 1px solid black;">'.$val['fecha'].'</td><td style="border: 1px solid black;">'.$val['dato'].'</td></tr>';
+       }
+    }
+} else {
+    $html = '<p>La serie no contiene información</p>';
+}
+echo $html;
